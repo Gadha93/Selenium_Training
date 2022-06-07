71b8f25ab041d2be9fa02974a6b44721892ca68e
Gadha Das <gadhada93@gmail.com>
2022-06-07 16:58:50 +0530

    first committed project

--- SeleniumProject/src/Tutorial/FlightTest.java
+++ SeleniumProject/src/Tutorial/FlightTest.java
@@ -2,6 +2,7 @@ package Tutorial;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -11,17 +12,29 @@ public class FlightTest {
 		// TODO Auto-generated method stub
 WebDriver driver=WebDriverManager.chromedriver().create();
 driver.manage().window().maximize();
-driver.get("https://www.booking.com/flights/index.en.html?");
+driver.get("https://www.booking.com/");
 Thread.sleep(3000);
+driver.findElement(By.linkText("Flights")).click();
+Thread.sleep(2000);
+
+//SOFT ASSERT
+String expected="Compare and book flights with ease";
+String actual=driver.findElement(By.xpath("//div[@class='Text-module__root--variant-headline_1___2W9ql']")).getText();
+System.out.println(actual);
+Assert.assertEquals(actual, expected);
+
 driver.findElement(By.xpath("//div[@data-testid='searchbox_controller_trip_type_ONEWAY']")).click();
-Thread.sleep(3000);
-driver.findElement(By.cssSelector("input[placeholder='Where from?']")).click();
+Thread.sleep(2000);
+//driver.findElement(By.cssSelector("input[placeholder='Where from?']")).click();
+//Thread.sleep(2000);
 //driver.findElement(By.xpath("//input[@data-testid='searchbox_origin_input']")).click();
 //Thread.sleep(3000);
 //driver.findElement(By.xpath("//div[@class='css-526oso']")).click();
 //driver.findElement(By.xpath("//div[@class='csst9qs2f']")).click();
 //Thread.sleep(2000);
-//driver.findElement(By.cssSelector("input[type='text']")).click();
+driver.findElement(By.cssSelector("input[readonly type='text']")).click();
+Thread.sleep(2000);
+driver.findElement(By.cssSelector("input[type='text']")).click();
 Thread.sleep(2000);
 
 
